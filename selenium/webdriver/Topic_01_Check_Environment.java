fa05dc239d7f46eb0feb3b520f7d05365a2a9ac2
Linh-Phan-KatOne <97660315+Linh-Phan-KatOne@users.noreply.github.com>
2024-01-19 16:25:09 +0700

    Add new repo

---
+++ selenium/webdriver/Topic_01_Check_Environment.java
@@ -0,0 +1,37 @@
+package webdriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class Topic_01_Check_Environment {
+    WebDriver driver;
+    String projectPath = System.getProperty("user.dir");
+
+    @Test
+    public void TC_01_Run_On_Firefox() {
+        driver = new FirefoxDriver();
+        driver.get("https://www.facebook.com/");
+        driver.quit();
+    }
+
+    @Test
+    public void TC_02_Run_On_Chrome() {
+        System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://www.facebook.com/");
+        driver.quit();
+    }
+
+    @Test
+    public void TC_03_Run_On_Edge() {
+        driver = new EdgeDriver();
+        driver.get("https://www.facebook.com/");
+        driver.quit();
+    }
+}
