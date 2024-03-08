e3716a9af68b38541abb1f856abd908085a8c6b2
ptgl <pgialinh.iuetv@gmail.com>
2024-03-08 18:44:30 +0700

    Add Selenium Locator 8 ways: id, class, name, tagname, linktext, partial linktext, css, xpath

--- selenium/webdriver/Topic_00_Template.java
+++ selenium/webdriver/Topic_00_Template.java
@@ -2,8 +2,6 @@ package webdriver;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
