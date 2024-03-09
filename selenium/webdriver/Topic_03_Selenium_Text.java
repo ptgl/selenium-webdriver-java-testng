f7fc5de07d5db0ff8558d37e5bc5ab6e876be840
ptgl <pgialinh.iuetv@gmail.com>
2024-03-09 10:46:18 +0700

    Add Selenium Locator 8 ways: id, class, name, tagname, linktext, partial linktext, css, xpath

--- selenium/webdriver/Topic_03_Selenium_Text.java
+++ selenium/webdriver/Topic_03_Selenium_Text.java
@@ -1,7 +1,9 @@
 package webdriver;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -14,23 +16,56 @@ public class Topic_03_Selenium_Text {
     public void beforeClass(){
         System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
         driver = new ChromeDriver();
-        driver.get("");
+        driver.get("https://automationfc.github.io/basic-form/");
     }
 
     @Test
-    public void TC_01() {
+    public void TC_01_Text_String() {
+
+        //Text() = can find text that:
+        //Absolute exact text, not cover the space
+        //Text at any index in the nested test
+        //<tag> text </child> </tag>
+        //<tag> </child> text </tag>
+        //<tag> </child> text <child>xyz</child> </tag> (child can contains other string)
+        driver.findElements(By.xpath("//h5[text()='Michael Jackson']"));
+
+        //Contains(Text(),..) can find text that:
+        //<tag>text<tag>
+        //<tag>   text can contain spaces  </tag>
+        //<tag> text <child></child> </tag> (text at the first index)
+        //NOT for:
+        //<tag> <child> text </child> </tag> (text in the child)
+        //<tag><child></child> text </tag> (text not at the first index)
+        //<tag> <child></child> text <child></child></tag>
+        driver.findElements(By.xpath("//h5[contains(text(),'Michael Jackson')]"));
+
+        // String()= or .=
+        //<tag>text</tag>
+        driver.findElements(By.xpath("//h5[string()='Michael Jackson']"));
+
+        //Contains(String(),..) or Contains(.,'azx')
+        //get all cases
+        driver.findElements(By.xpath("//h5[contains(string(),'Michael Jackson')]"));
+
+
+        System.out.println(driver.findElement(By.id("nested")).getText());
+
 
 
     }
 
     @Test
-    public void TC_02() {
-
+    public void TC_02_Concat() {
+        String concatText = driver.findElement(By.xpath("//h5/span[text()=concat('Hello \"John\", What',\"'s happened?\")]")).getText();
+        Assert.assertEquals(concatText, "Hello \"John\", What's happened?");
     }
 
     @Test
-    public void TC_03() {
-
+    public void TC_03_And_Or_Not() {
+        driver.findElement(By.xpath("//input[@type='email' and @id='mail']")).isDisplayed();
+        driver.findElements(By.xpath("//input[@type='email' or @type='number']"));
+        driver.findElements(By.xpath("//input[not(@type='email')]"));
     }
 
     @AfterClass
