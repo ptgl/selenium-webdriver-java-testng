055a36d3e8df9520ea9b41ae4356e9b7b2eb6779
ptgl <pgialinh.iuetv@gmail.com>
2024-04-27 20:16:48 +0700

    Add topic 17 Shadow DOM

---
+++ selenium/webdriver/Topic_17_Shadow_DOM.java
@@ -0,0 +1,60 @@
+package webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Topic_17_Shadow_DOM {
+    WebDriver driver;
+
+    @BeforeClass
+    public void beforeClass(){
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
+    }
+
+    @Test
+    public void TC_01() {
+        driver.get("https://automationfc.github.io/shadow-dom/");
+        WebElement shadowHost = driver.findElement(By.id("shadow_host"));
+        SearchContext shadowRoot = shadowHost.getShadowRoot();
+        Assert.assertEquals(shadowRoot.findElement(By.cssSelector("#shadow_content>span")).getText(),"some text");
+        Assert.assertFalse(shadowRoot.findElement(By.cssSelector("input[type='checkbox']")).isSelected());
+
+        shadowHost = shadowRoot.findElement(By.cssSelector("#nested_shadow_host"));
+        shadowRoot = shadowHost.getShadowRoot();
+        Assert.assertEquals(shadowRoot.findElement(By.cssSelector("#nested_shadow_content>div")).getText(),"nested text");
+
+    }
+
+    @Test
+    public void TC_02_shopee() {
+        driver.get("https://shopee.vn/");
+
+    }
+
+
+
+    @AfterClass
+    public void afterClass(){
+        driver.quit();
+    }
+
+    private void sleepInSeconds(long seconds){
+        try {
+            Thread.sleep(seconds*1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
